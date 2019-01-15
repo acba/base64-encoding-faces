bdfec77dba13949348bbe5de2f3e201eb5337c50
Augusto Almeida <augusto.almeida@mppb.mp.br>
2019-01-15 18:30:30 -0300

    Escrevendo em arquivo e nao em BD

--- gera_base.py
+++ gera_base.py
@@ -5,17 +5,16 @@ import csv
 import base64
 import sqlite3
 
-import numpy as np
 import pandas as pd
 import face_recognition
 
-number_of_workers = 16
+number_of_workers = 8
 
 data_path = './data'
 if not os.path.exists(data_path):
   os.makedirs(data_path)
 
-connection = sqlite3.connect(os.path.join(data_path,'db.sqlite3'))
+# connection = sqlite3.connect(os.path.join(data_path,'db.sqlite3'))
 
 def __run(idx, filename, path):
   if filename.endswith(".jpg"):
@@ -25,28 +24,29 @@ def __run(idx, filename, path):
     img = face_recognition.load_image_file(filepath)
     hash = hashlib.sha256(img).hexdigest().upper()
 
-    res = connection.execute(f'SELECT EXISTS(SELECT 1 FROM encodings WHERE hash=?)', (hash,))
-    existe, = res.fetchone()
-   
-    if not existe:
-      encodings = face_recognition.face_encodings(img)
-      if len(encodings) == 1:
-        encoding = encodings[0]
-        byte_encoding = encoding.tostring()
-        str_base64_encoding = base64.b64encode(byte_encoding).decode('utf-8')
+    # res = connection.execute(f'SELECT EXISTS(SELECT 1 FROM encodings WHERE hash=?)', (hash,))
+    # existe, = res.fetchone()
 
-        connection.execute('INSERT INTO encodings VALUES (?,?,?)', (hash, cpf, str_base64_encoding))
-        connection.commit()
-        return True
-        # return {
-        #   'cpf': cpf,
-        #   'encoding': str_base64_encoding,
-        #   'hash': hash
-        # }
-      elif len(encodings) > 1:
-        print(f'Arquivo {filepath} não pode ser codificado, mais de um rosto detectado - concluído.')
-      else:
-        print(f'Arquivo {filepath} não pode ser codificado - concluído.')
+    # if not existe:
+    encodings = face_recognition.face_encodings(img)
+    if len(encodings) == 1:
+      encoding = encodings[0]
+      byte_encoding = encoding.tostring()
+      str_base64_encoding = base64.b64encode(byte_encoding).decode('utf-8')
+
+      # connection.execute('INSERT INTO encodings VALUES (?,?,?)', (hash, cpf, str_base64_encoding))
+      # connection.commit()
+      # return True
+
+      return {
+        'cpf': cpf,
+        'encoding': str_base64_encoding,
+        'hash': hash
+      }
+    elif len(encodings) > 1:
+      print(f'Arquivo {filepath} não pode ser codificado, mais de um rosto detectado - concluído.')
+    else:
+      print(f'Arquivo {filepath} não pode ser codificado - concluído.')
 
 
   return None
@@ -55,15 +55,15 @@ def process(rootpath):
   paths = os.listdir(rootpath)
   print(f'Pastas de fotos: {paths}')
 
-  connection.execute('CREATE TABLE IF NOT EXISTS encodings(hash, cpf, encoding)')
+  # connection.execute('CREATE TABLE IF NOT EXISTS encodings(hash, cpf, encoding)')
 
   with open(os.path.join(data_path, 'encodings.csv'), mode='w') as f:
     fieldnames = ['hash', 'cpf', 'encoding']
     writer = csv.DictWriter(f, fieldnames=fieldnames)
 
     writer.writeheader()
-      
-    for p in paths[:1]:
+
+    for p in paths:
       path = os.path.join(rootpath, p)
       n_files = len(os.listdir(path))
       print(f'Processando {n_files} arquivos de {path}')
@@ -73,10 +73,10 @@ def process(rootpath):
 
       with multiprocessing.Pool(number_of_workers) as p:
         encodings = p.starmap(__run, entradas)
-        # encodings = list(filter(lambda x: x is not None, encodings))
-        # tuples = [(e['cpf'], e['hash'], e['encoding']) for e in encodings]
-        
-        # writer.writerows(encodings)
+        encodings = list(filter(lambda x: x is not None, encodings))
+        tuples = [(e['cpf'], e['hash'], e['encoding']) for e in encodings]
 
-rootpath = '/home/acba/workspace/github/siap-reconhecimentofacial/data/fotos'
+        writer.writerows(encodings)
+
+rootpath = '/home/cesar/workspace/dados/fotos_sem_assinatura'
 process(rootpath)
--- read.py
+++ read.py
@@ -1,16 +1,42 @@
 import pandas as pd
 import numpy as np
 import base64
+import time
 
-df = pd.read_csv('./encodings.csv')
+def base64_to_numpy(data):
+  _bytes = data.encode('utf-8')
+  return np.frombuffer(base64.b64decode(_bytes), dtype=np.float64)
+
+ti = time.time()
+df = pd.read_csv('./data/encodings.csv')
+print(f'Carregamento da base em {time.time() - ti} s')
+
 print(df.head())
 print(df.dtypes)
-str_base = df['encoding'].iloc[0]
-byte_base = str_base.encode('utf-8')
+# str_base = df['encoding'].iloc[0]
+# byte_base = str_base.encode('utf-8')
 
-e = base64.b64decode(byte_base)
-print(np.frombuffer(e, dtype=np.float64))
+# e = base64.b64decode(byte_base)
+# print(np.frombuffer(e, dtype=np.float64))
 # print(df['encoding'].iloc[0])
 # encoding = df['encoding'].iloc[0]
 
 # print(np.frombuffer(encoding, dtype=np.float64))
+
+ti = time.time()
+arr = [ base64_to_numpy(r) for r in df['encoding']]
+print(f'Processamento da base em {time.time() - ti} s')
+
+ti = time.time()
+arr = [ {'cpf': r[2], 'encoding': base64_to_numpy(r[3]) } for r in df.to_records()]
+print(f'Processamento da base em {time.time() - ti} s')
+
+ti = time.time()
+arr_enc = [ base64_to_numpy(r) for r in df['encoding']]
+arr_cpf = [ r for r in df['cpf']]
+arr = [ {'cpf': arr_cpf[idx], 'encoding': enc } for idx, enc in enumerate(arr_enc)]
+print(f'Processamento da base em {time.time() - ti} s')
+
+ti = time.time()
+arr = [ { 'cpf': r['cpf'], 'encoding': base64_to_numpy(r['encoding']) } for idx, r in df.iterrows()]
+print(f'Processamento da base em {time.time() - ti} s')
